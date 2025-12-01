7de0ce38854290acd64097065c39f361c999b98b
Null-Anomaly <lbryant019@insite.4cd.edu>
2025-11-30 21:30:20 -0800

    30 minutes

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -10,14 +10,15 @@ using namespace std;
 int main() {
     
     stringBinaryTree tree;
-    ifstream inner ("codes.txt");
-    inner.open("codes.txt");
+    ifstream fin ("codes.txt");
+    fin.open("codes.txt");
     string line;
-    while(getline(inner, line))
+    while(fin.good())
     {
+        getline(fin, line);
         tree.insertNode(line);
     }
-    inner.close();
+    fin.close();
     
     int x = -1;
 
@@ -27,28 +28,28 @@ int main() {
         cout << "1. Insert a string into the tree\n";
         cout << "2. Delete a string from the tree\n";
         cout << "3. Search for a string in the tree\n";
-        cout << "4. Modify the records\n";
+        cout << "4. Display Records in order\n";
         cout << "0 to exit\n";
         cin >> x;
 
         if(x == 1)
         {
             string temp;
-            cout << "Insert the string you want to add";
+            cout << "Insert the string you want to add: ";
             cin >> temp;
             tree.insertNode(temp);
         }
         if(x == 2)
         {
             string temp;
-            cout << "Insert the value of the string that you want to delete";
+            cout << "Insert the value of the string that you want to delete: ";
             cin >> temp;
             tree.remove(temp);
         }
         if( x == 3)
         {
             string temp;
-            cout << "Insert the value of the string that you want to search for";
+            cout << "Insert the value of the string that you want to search for: ";
             cin >> temp;
             if(tree.searchNode(temp))
             {
@@ -59,6 +60,11 @@ int main() {
                 cout << "The string " << temp << " was not found in the tree.\n";
             }
         }
+        if(x == 4)
+        {
+            tree.displayInOrder();
+        }
+
     }
 
     return 0;
